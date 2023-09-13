7b4674b2a9d0ae711fc114b2880bbaeab0aced71
_Corentin_ <solois.corentin@gmail.com>
2023-09-13 15:03:32 +0200

    change IP

--- client.py
+++ client.py
@@ -1,15 +1,15 @@
+from programe_game.game import main
+import os
 import socket
 
 
 connexion = socket.socket()
-host = '138.68.96.66'
+host = 'localhost'
 port = 2004
 connexion.connect((host, port))
 
-import os
 # récupérer le chemin du répertoire courant.
 path = os.getcwd()
 
-from programe_game.game import main
 
 main(connexion, path)
